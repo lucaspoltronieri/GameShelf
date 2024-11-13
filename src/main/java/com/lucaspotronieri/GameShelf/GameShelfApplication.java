4b07d72513490d4bb43602ecda61976b4db371e5
Lucas Poltronieri <lucaspoltronierisampaio@gmail.com>
2024-11-13 14:08:28 -0300

    Project list of games created

---
+++ src/main/java/com/lucaspotronieri/GameShelf/GameShelfApplication.java
@@ -0,0 +1,13 @@
+package com.lucaspotronieri.GameShelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameShelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucaspotronieri/GameShelf/GameShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaspotronieri.GameShelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
